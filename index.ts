9d91e2b6f9efe304caba5ca4bd0f5fb26aa7f047
John Razmus <john@razmus.net>
2018-01-09 21:37:34 -0600

    Update and rename index.js to index.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+// exports.IonRangeSliderModule = require('./lib/ion-range-slider.module').IonRangeSliderModule;
+// exports.IonRangeSliderComponent = require('./lib/ion-range-slider.component').IonRangeSliderComponent;
+
+export { IonRangeSliderModule } from './src/ion-range-slider.module'
+export { IonRangeSliderComponent } from './src/ion-range-slider.component'
