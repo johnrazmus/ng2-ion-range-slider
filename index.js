94244db8f66e1871c1062f8f0689b83e67c426eb
John Razmus <john@razmus.net>
2018-01-09 23:02:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// exports.IonRangeSliderModule = require('./lib/ion-range-slider.module').IonRangeSliderModule;
-// exports.IonRangeSliderComponent = require('./lib/ion-range-slider.component').IonRangeSliderComponent;
+exports.IonRangeSliderModule = require('./lib/ion-range-slider.module').IonRangeSliderModule;
+exports.IonRangeSliderComponent = require('./lib/ion-range-slider.component').IonRangeSliderComponent;
 
-export { IonRangeSliderModule } from './lib/ion-range-slider.module'
-export { IonRangeSliderComponent } from './lib/ion-range-slider.component'
+// export { IonRangeSliderModule } from './lib/ion-range-slider.module'
+// export { IonRangeSliderComponent } from './lib/ion-range-slider.component'
