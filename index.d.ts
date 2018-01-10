301a9f12a53deed7f1dc9f85d03640be218373ff
John Razmus <john@razmus.net>
2018-01-09 23:04:15 -0600

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export { IonRangeSliderModule } from './lib/ion-range-slider.module'
-export { IonRangeSliderComponent } from './lib/ion-range-slider.component'
+export * from './lib/ion-range-slider.module'
+export * from './lib/ion-range-slider.component'
